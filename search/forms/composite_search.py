774a1cd95785884f239e9b3952b446812c92e644
Shirren Premaratne <shirren.premaratne@mq.edu.au>
2012-09-26 18:11:35 +1000

    Switched search form to use Django forms

---
+++ search/forms/composite_search.py
@@ -0,0 +1,10 @@
+from django import forms
+from django.forms.widgets import CheckboxSelectMultiple
+
+# Models in use
+from search.models.sites import Sites
+
+class CompositeSearchForm (forms.Form):
+	""" Form used to represent the composite search fields """
+	sites = Sites.objects.all ()
+	sites_field = forms.ModelMultipleChoiceField(widget=CheckboxSelectMultiple(), required = True, queryset = sites)
--- search/views/composite.py
+++ search/views/composite.py
@@ -1,27 +1,21 @@
-from django.shortcuts import get_object_or_404, render_to_response
+from django.shortcuts import render, render_to_response
 from django.http import HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse
 from django.template import RequestContext
 
 # Models in usr
-from search.models.sites import Sites
+from search.forms.composite_search import CompositeSearchForm
 
 def index (request):
 	""" The composite search view returns pretty much all the standard fieldsets in a simplified
 	view. This view is meant for normal users, not power users. """
-	sites = Sites.objects.all ()
-	return render_to_response ('composite/index.html', {'sites': sites})
-
+	form = CompositeSearchForm ()
+	return render (request, 'composite/index.html', {'form': form})
 
 def search (request):
 	""" This function shows the results of a search. """
-	try:
-		sites = request.GET['site']
-	except (KeyError):
-		return render_to_response ('composite/index.html', {
-                'sites': Sites.objects.all (),
-                'error_message': "You didn't select any search criteria.",
-            }, context_instance = RequestContext (request)
-            )
-	else:
-		return render_to_response ('composite/results.html', {'sites': sites})
+	form = CompositeSearchForm (request.GET)
+	if form.is_valid ():
+		return render_to_response ('composite/results.html', {'results': form.cleaned_data['sites_field']})
+
+
