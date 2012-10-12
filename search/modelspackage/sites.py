1998a04cfe334641d2261f5e789f2cf5130ab82d
Shirren Premaratne <shirren.premaratne@gmail.com>
2012-10-12 12:08:52 +1100

    Added get_absolute_url to the site model.

--- search/modelspackage/sites.py
+++ search/modelspackage/sites.py
@@ -70,7 +70,7 @@ class Site (SparqlModel):
 
     def get_absolute_url(self):
         """Return a canonical URL for this item"""    
-        return "/browse/%s/" % (self.site)   
+        return "/browse/%s/" % (self.name)   
 
 
     def __unicode__ (self):
