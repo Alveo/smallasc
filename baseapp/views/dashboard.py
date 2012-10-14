a062093bf72fabe7a5bb0041e7ca91ace757ab26
stevecassidy <stevecassidy@localhost>
2012-10-15 09:39:57 +1100

    don't require login for main page

--- baseapp/views/dashboard.py
+++ baseapp/views/dashboard.py
@@ -3,7 +3,6 @@ from django.shortcuts import render, render_to_response
 from django.template import RequestContext
 
 
-@login_required
 def index (request):
 	""" Render the dashboard """
 	return render_to_response ('index.html', RequestContext (request))
