800731023f7688455f44ae9c4042ced9957b6565
stevecassidy <stevecassidy@localhost>
2012-10-18 13:29:48 +1100

    don't put Site in admin - caused crash on server and we don't need the admin pages anyway

--- browse/admin.py
+++ browse/admin.py
@@ -2,4 +2,4 @@ from django.contrib import admin
 from browse.modelspackage.sites import Site
 
 # Register the sites we would like to appear in the administration application
-admin.site.register(Site)
+#admin.site.register(Site)
