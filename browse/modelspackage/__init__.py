f86bb0e3c79eacc70d9b3a4d33ffa4be271b49b2
Steve Cassidy <steve.cassidy@mq.edu.au>
2012-10-22 12:34:31 +1100

    Add simple search based on the text in a prompt. Returns a page with a list of items.

--- browse/modelspackage/__init__.py
+++ browse/modelspackage/__init__.py
@@ -7,6 +7,10 @@ from participants import Participant
 from education_history import EducationHistory
 from professional_history import ProfessionalHistory
 from media import Media
-from sparql_local_wrapper import SparqlLocalWrapper
+from sparql_local_wrapper import SparqlMixin, SparqlManager, SparqlModel
+from protocol import Protocol
 
-__all__ = ['Component', 'Site', 'Session', 'Item', 'Location', 'Participant', 'EducationHistory', 'ProfessionalHistory', 'Media', 'SparqlLocalWrapper']
+__all__ = ['Protocol', 'Component', 'Site', 'Session', 'Item', 
+		'Location', 'Participant', 'EducationHistory', 
+		'ProfessionalHistory', 'Media', 
+		'SparqlMixin', 'SparqlManager', 'SparqlModel']
