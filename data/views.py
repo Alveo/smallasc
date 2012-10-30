d4804dc1570b9ebf81b912c7cbdc73a968db232d
Tomas Krajca <t.l.krajca@gmail.com>
2012-10-30 12:16:11 +0100

    Quickly hacked integration between components browse and download endpoint as a proof of concept

--- data/views.py
+++ data/views.py
@@ -14,6 +14,7 @@ import os
 
 from data import tasks
 from data.models import Files2taskId
+from browse.modelspackage.sparql_local_wrapper import SparqlManager
 
 
 def _unify_hash(l):
@@ -25,17 +26,49 @@ def _removeDataHostPrefix(url):
 
 @csrf_exempt
 def download_redirect(request):
+        # TODO: this is just a quick hack to show that this approach works, I'll
+        # tidy it up if we decide to go this way
         if request.method == 'POST':
-                """{ 'media' : [<url>+], 'name' : \w+ }"""
+                """name=\w+&channel=<channel>&json={ 'media' : [<url>+], channels' : [<channel>+], 'name' : \w+ }"""
                 try:
-                        data = simplejson.loads(request.raw_post_data)
+                        data = simplejson.loads(request.POST.get("json"))
                 except ValueError:
                         return HttpResponseServerError("Malformed json object")
 
-                if not 'media' in data or not 'name' in data:
-                      return HttpResponseServerError("Invalid json query syntax - should be { 'media' : [<url>+], 'name' : \w+ }")
+                if not 'media' in data:
+                        return HttpResponseServerError('Invalid json query syntax - should be { "media" : [<url>+], "channels" : [<channel>+], "name" : "\w+" }')
+
+                if not 'name' in data:
+                        # we know how to handle an empty name
+                        data['name'] = request.POST.get("name", "")
+
+                channel = request.POST.get("channel")
+                data['files'] = [ d for d in data['media'] if d.startswith(settings.DATA_HOST_PREFIX) ]
+                if channel is not None:
+                        s = SparqlManager()
+
+                        q = """?item dc:isPartOf <%s> .
+                ?item austalk:media ?media .
+                ?media austalk:version 1 .
+                ?media austalk:channel "%s" .
+        """
+
+                        qq = """select distinct ?media
+where {
+        {
+                %s
+        }
+}
+                        """ % " } UNION {\n\t\t".join(
+                                        [ q % (d, channel) for d in data['media'] if not d.startswith(settings.DATA_HOST_PREFIX) ])
+                        #print qq
+
+                        files = s.query(qq)
+                        data['files'] += [ f['media']['value'] for f in files['results']['bindings'] ]
                
-                urlPaths = map(_removeDataHostPrefix, data['media'])
+                #print "Number of data file urls:", len(data['files'])
+
+                urlPaths = map(_removeDataHostPrefix, data['files'])
 
                 h = _unify_hash(urlPaths)
                 try:
@@ -62,7 +95,9 @@ def download(request, h):
         task = get_object_or_404(Files2taskId, h=h)
         res = celery.result.AsyncResult(task.task_id)
 
-        name = request.GET.get("name", "smallasc-search-data.zip")
+        name = request.GET.get("name", "")
+        if len(name) == 0:
+                name = "smallasc-search-data.zip"
         if not name.endswith(".zip"):
                name += ".zip"
         started_at = task.dt
