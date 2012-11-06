d3bc10aa739650ef7dbb4e1c8d265c2668bf48f3
Tomas Krajca <t.l.krajca@gmail.com>
2012-11-06 10:06:10 +0100

    Creating new directory structure

--- baseapp/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import patterns, include, url
-from baseapp.views.security import logout
-
-urlpatterns = patterns('baseapp.views',
-    # Security related pages
-    (r'^logout/$', logout),
-)
--- baseapp/views/dashboard.py
+++
@@ -1,8 +0,0 @@
-from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, render_to_response
-from django.template import RequestContext
-
-
-def index (request):
-	""" Render the dashboard """
-	return render_to_response ('index.html', RequestContext (request))
