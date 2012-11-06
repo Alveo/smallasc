26f30f4f7d8e174fda2fb12a0b923dca295b3681
Tomas Krajca <t.l.krajca@gmail.com>
2012-11-06 11:34:40 +0100

    Removed data application

--- data/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class Files2taskId(models.Model):
-    h = models.CharField(max_length=128)
-    task_id = models.CharField(max_length=128)
-    dt = models.DateTimeField(auto_now_add=True)
-
