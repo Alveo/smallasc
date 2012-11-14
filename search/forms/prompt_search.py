f1b51c61cd181dfb394431979fc61003ac826018
stevecassidy <stevecassidy@localhost>
2012-11-14 14:22:02 +1100

    Rename search views, forms and templates so the names make sense. Add start of participant search form.

--- search/forms/prompt_search.py
+++ search/forms/prompt_search.py
@@ -11,13 +11,27 @@ def component_choices():
     
     return p.components() 
 
-class CompositeSearchForm (forms.Form):
+QA_AUDIO_CHOICES = (('any', 'Any'),
+                    ('A', 'A'),
+                    ('B', 'B'),
+                    ('C', 'C'),
+                    ('D', 'D'))
+
+class PromptSearchForm (forms.Form):
     """ Form used to represent the composite search fields """
     # Construct the sites choice list in the form
 
     prompt = forms.CharField(label='Prompt contains')
     wholeword = forms.BooleanField(label="Match whole word", required=False)
+
+    
+class ComponentSearchForm(forms.Form):
+    """Form to select items by component """
+    
     components = forms.MultipleChoiceField(widget     = CheckboxSelectMultiple (),
                                            label       = 'Components',
                                            required    = False,
                                            choices     = component_choices())
+    
+    qa_audio = forms.Choicefield(label="Audio Quality", 
+                                 choices=QA_AUDIO_CHOICES)
--- search/urls.py
+++ search/urls.py
@@ -4,6 +4,6 @@ from django.views.generic import DetailView, ListView
 # Generic patterns for search
 urlpatterns = patterns('search.views',
     
-    url (r'^$', 'composite.index'),
-    url (r'^results/$', 'composite.search')
+    url (r'^$', 'index'),
+    url (r'^results/$', 'prompt.prompt_search')
 )
