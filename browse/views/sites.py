a3d023653b8d4c35345cca8aef61b908960557ed
stevecassidy <stevecassidy@localhost>
2012-11-21 13:41:34 +1100

    Surf based models, convert site and participant views to surf.

--- browse/views/sites.py
+++ browse/views/sites.py
@@ -1,14 +1,13 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, render_to_response
 
-from browse.modelspackage.sites import Site
-
+from browse.modelspackage.surfmodels import RecordingSite
 
 @login_required
 def index (request):
     """ The sites index view displays all the available sites current in the RDF store. """
 
-    sites = Site.objects.all ()
+    sites = RecordingSite.objects().all()
     return render (request, 'browse/sites/index.html', {'sites': sites})
 
 
