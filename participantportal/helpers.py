f237e336e440d0acdb0418245e5d840fbc3b5a26
Shirren Premaratne <shirren2010@gmail.com>
2012-11-30 17:11:52 +1100

    Started work on showing users data

--- participantportal/helpers.py
+++ participantportal/helpers.py
@@ -1,3 +1,5 @@
+from urlparse import urlparse
+
 def read_model_property(model, key):
   """ This helper method is used to avoid having to explicitly specify
   keys and the 0'th index position each time we want to extract a value. """
@@ -5,3 +7,8 @@ def read_model_property(model, key):
     return model.properties()[key][0]
   else:
     return None
+
+def get_site_id_from_url(site_url):
+  components = urlparse(site_url)
+  path_components = components.path.split('/')
+  return path_components[len(path_components) - 1]
