98abbf50ee0bad8b90bb910f6ccf38436b8f43d2
Shirren Premaratne <shirren2010@gmail.com>
2012-11-30 17:11:52 +1100

    Started work on showing users data

--- participantportal/tests.py
+++ participantportal/tests.py
@@ -1,5 +1,8 @@
+from browse.modelspackage.participants import *
+from browse.modelspackage.sites import *
 from django.test import TestCase
 from participantportal.modelspackage.auth import CustomAuthBackend
+from participantportal.helpers import *
 
 class AuthenticationTests(TestCase):
     
@@ -10,5 +13,15 @@ class AuthenticationTests(TestCase):
 
   def test_authentication_for_invalid_user(self):
     auth_backend = CustomAuthBackend()
-    user = auth_backend.get_user('1_62')
+    user = auth_backend.authenticate('1', '62')
     self.assertIsNone(user)
+
+  def test_retrieve_participant_data(self):
+    participant = Participant.objects.get('1_1093')
+    self.assertIsNotNone(participant)
+    # print participant.properties()
+
+  def test_retrieve_site(self):
+    site_id = get_site_id_from_url('http://id.austalk.edu.au/protocol/site/UTAS')
+    site = Site.objects.get(site_id)
+    self.assertIsNotNone(site)
