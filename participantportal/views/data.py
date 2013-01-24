aa5674705b1bf1f208c67df889beccbaaf3e7dd8
Shirren Premaratne <shirren2010@gmail.com>
2013-01-24 15:20:14 +1100

    Added terms and conditions, I am sure it requires more work.

--- participantportal/views/data.py
+++ participantportal/views/data.py
@@ -2,18 +2,14 @@ from django.contrib.auth.decorators import login_required
 from django.http import Http404
 from django.shortcuts import render, render_to_response
 from django.template import RequestContext
-
 from browse.modelspackage.participants import Participant
 from browse.modelspackage.sessions import Session
 from browse.modelspackage.residence_history import ResidenceHistory
 from browse.modelspackage.language_usage import LanguageUsage
-
 from participantportal.settings import *
-
 
 @login_required(login_url = PP_LOGIN_URL)
 def index (request):
-
   participant = Participant.objects.get (request.user)
   if participant is None:
       raise Http404("Participant not found")
@@ -22,8 +18,7 @@ def index (request):
   rhist = ResidenceHistory.objects.filter_by_participant(participant)
   lang = LanguageUsage.objects.filter_by_participant(participant)
 
-  return render (request, 'home.html', {
-            'user_profile': request.user.userprofile,
+  return render (request, 'data.html', {
             'participant': participant,
             'sessions': sessions,
             'residential_history': rhist,
