fbb4342db815ecdf224fdba6b58735c32832b6a4
Tomas Krajca <t.l.krajca@gmail.com>
2013-03-08 15:43:10 +0800

    Remove the data app, it's not supposed to be here any more - forked into a separate project

--- data/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls.defaults import patterns, include, url
-
-
-urlpatterns = patterns('data.views',
-    url(r'^$', 'download_redirect', name='download_redirect'),
-    url(r'^(?P<h>.*)/$', 'download', name='download'),
-)
-
