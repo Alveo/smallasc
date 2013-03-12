dadf146fb3e6f0590ca4426516a445196db402cc
Shirren Premaratne <shirren2010@gmail.com>
2013-03-12 17:05:05 +1100

    Refactored out some complex functionality in the component search view.

--- search/views/components.py
+++ search/views/components.py
@@ -10,33 +10,31 @@ from search.forms import ParticipantSearchForm, ParticipantSearchFilterForm, Par
 @permission_required('auth.can_view_item_search') 
 def search (request):
 
-    search_form = ParticipantSearchForm (request.GET)
-    participants = Participant.objects.filter (search_form.generate_predicates())
-    participant_form = ParticipantSearchFilterForm (participants, request.GET)
+    search_form         = ParticipantSearchForm (request.GET)
+    participants        = Participant.objects.filter (search_form.generate_predicates())
+    participant_form    = ParticipantSearchFilterForm (participants, request.GET)
 
-    # If the participant form is valid then we have all the data we need
-    # so we can render the component form
-    if participant_form.is_valid ():
+    if participant_form.is_valid():
+
         components = []
-        for part in participant_form.return_selected_participants():
-            components += Component.objects.filter_by_participant(part)
+        for selected_participant in participant_form.return_selected_participants():
+            components += Component.objects.filter_by_participant(selected_participant)
 
-        component_form = ParticipantComponentSearchForm (
-            participants,
-            sorted (set(components), key = lambda comp: comp.sessionId),
-            request.GET)
+        components_sorted = sorted (set(components), key = lambda comp: comp.sessionId)
+        component_form = ParticipantComponentSearchForm (participants, components_sorted, request.GET)
 
-        if component_form.is_valid ():
+        if component_form.is_valid():
+
             items = []
             for comp in components:
                 if comp.identifier in component_form.cleaned_data["components_field"]:
                     items += Item.objects.filter_by_component (comp.participantId, comp.sessionId, comp.componentId) 
 
-            item_ids = [item.identifier for item in items]
-            return render (request, 'search/results.html', { 'items': items, 'item_ids': item_ids })
+            return render (request, 'search/results.html', { 'items': items, 'item_ids': [item.identifier for item in items] })
+
         else:
+
             return render(request, 'search/index.html', { 'form': component_form })
     else:
-        # If we reach here then the user has adjusted the initial search criteria and the
-        # participant form is no longer valid so we need to re-render it
+
         return render(request, 'search/index.html', {'form': participant_form})
