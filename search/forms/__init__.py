55a6c30c30eaaaf7afc95d2f60b221c6f83d4c65
shirren <shirren@localhost>
2013-03-13 17:41:35 +1100

    Participant filter is broken, need to fix it, WIP.

--- search/forms/__init__.py
+++ search/forms/__init__.py
@@ -1,4 +1,5 @@
 from prompt_search                  import PromptSearchForm
+
 from participant_search             import ParticipantSearchForm
-from participant_search             import ParticipantSearchFilterForm
+from participant_search_filter      import ParticipantSearchFilterForm
 from participant_components_search  import ParticipantComponentSearchForm
--- search/views/components.py
+++ search/views/components.py
@@ -11,23 +11,28 @@ from search.helpers                 import append_querystring_to_url
 @permission_required('auth.can_view_item_search') 
 def search(request):
 
-    search_form         = ParticipantSearchForm(request.GET)
-    all_participants    = Participant.objects.filter(search_form.generate_predicates())
-    participants        = user_filtered_participants(request, all_participants)
+    search_form             = ParticipantSearchForm(request.GET)
+    all_participants        = Participant.objects.filter(search_form.generate_predicates())
+    filtered_participants   = user_filtered_participants(request, all_participants)
 
     components = []
-    for participant in participants:
+    for participant in filtered_participants:
         components += Component.objects.filter_by_participant(participant.friendly_id())
 
-    components_sorted   = sorted (set(components), key = lambda comp: comp.sessionId)
-    component_form      = ParticipantComponentSearchForm (participants, components_sorted, request.GET)
+
+    components_sorted   = sorted(set(components), key = lambda comp: comp.sessionId) # The use of set ensures items are unique
+    component_form      = ParticipantComponentSearchForm(filtered_participants, components_sorted, request.GET)
 
     if component_form.is_valid():
 
         items = []
-        for comp in components:
-            if comp.identifier in component_form.cleaned_data["components_field"]:
-                items += Item.objects.filter_by_component (comp.participantId, comp.sessionId, comp.componentId) 
+
+        for comp in component_form.return_selected_components():
+            for participant in filtered_participants:
+            
+                print "Component %s with participant identifier %s" % (comp.identifier, participant.friendly_id())
+
+                items += Item.objects.filter_by_component(participant.friendly_id(), comp.sessionId, comp.componentId) 
 
         return render (request, 'search/results.html', { 
             'items': items, 
@@ -38,7 +43,7 @@ def search(request):
 
         return render(request, 'search/components.html', { 
             'form':                     component_form, 
-            'participant_count':        len(participants),
+            'participant_count':        len(filtered_participants),
             'participant_url':          append_querystring_to_url(request, '/browse/participants'),
             'participant_filter_url':   append_querystring_to_url(request, '/search/participants')
         }) # Should use reverse urls instead of hard coded urls
@@ -49,8 +54,13 @@ def user_filtered_participants(request, all_participants):
     
     participant_form    = ParticipantSearchFilterForm(all_participants, request.GET)
 
+    print request.GET
+
     if participant_form.is_valid():
 
+        print "User filtered participants %s" % (participant_form.return_selected_participants())
+
         return participant_form.return_selected_participants()
+
 
     return all_participants
