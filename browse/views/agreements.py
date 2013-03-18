a5f093ea3fff65c0d22a81c4dc50bd66164de67c
Shirren Premaratne <shirren2010@gmail.com>
2013-03-18 12:41:27 +1100

    Adding features to search.

--- browse/views/agreements.py
+++ browse/views/agreements.py
@@ -1,11 +1,16 @@
-from django.http import Http404
+from django.http                    import Http404
 from django.contrib.auth.decorators import login_required, permission_required
-from django.shortcuts import render
-from participantportal.models import *
+from django.shortcuts               import render
+
+from participantportal.models       import *
+
 
 @login_required
 @permission_required('auth.can_view_agreements')
 def index (request):
+
     participants = UserProfile.objects.all()
-    return render (request, 'browse/agreements/index.html', {'participants' : participants })
+    return render (request, 'browse/agreements/index.html', {
+        'participants' : participants 
+    })
 
--- browse/views/sites.py
+++ browse/views/sites.py
@@ -1,14 +1,16 @@
 from django.contrib.auth.decorators import login_required, permission_required
-from django.shortcuts import render
+from django.shortcuts               import render
 
-from browse.modelspackage.sites import Site
+from browse.modelspackage.sites     import Site
+
 
 @login_required
 @permission_required('auth.can_view_sites')
 def index (request):
-    """ The sites index view displays all the available sites current in the RDF store. """
 
     sites = Site.objects.all ()
-    return render (request, 'browse/sites/index.html', {'sites': sites})
+    return render (request, 'browse/sites/index.html', {
+        'sites': sites
+    })
 
 
