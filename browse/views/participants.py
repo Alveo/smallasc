1f229798fa7e89e89ccf9c6462733d67f1aae798
Steve Cassidy <steve.cassidy@mq.edu.au>
2016-01-14 14:33:35 +1100

    Implement caching of query results using the Django cache framework. Improve page layout for sites and participants.

--- browse/views/participants.py
+++ browse/views/participants.py
@@ -22,13 +22,13 @@ def index(request, site_id):
         participants = Participant.objects.filter(search_form.generate_predicates())
     else:
         participants = Participant.objects.with_data(site)
-    
+
     breadcrumbs = generate_breadcrumbs(request,site)
 
     return render(request, 'browse/participants/index.html', {
         'request'       : request,
         'site'          : site,
-        'participants'  : generate_paginated_object(request, participants),
+        'participants'  : participants,
         'breadcrumbs' : breadcrumbs
     })
 
@@ -37,19 +37,19 @@ def index(request, site_id):
 @login_required
 @permission_required('auth.can_view_participant')
 def show_by_id(request, participant_id):
-    """View of participant given just the identifier - so we can resolve 
+    """View of participant given just the identifier - so we can resolve
     redirects from id.austalk.edu.au"""
-        
+
     participant = Participant.objects.get (participant_id)
-    
+
     site = participant.get_site()
-    
+
     return HttpResponseRedirect(reverse('browse.views.participants.show', args=(site, participant_id)))
 
 @login_required
 @permission_required('auth.can_view_participant')
 def show(request, site_id, participant_id):
-        
+
     site = Site.objects.get (site_id)
 
     if site is None:
@@ -61,7 +61,7 @@ def show(request, site_id, participant_id):
     if participant is None:
         raise Http404 ("Requested participant not found")
 
-    sessions = Session.objects.filter_by_participant (participant) 
+    sessions = Session.objects.filter_by_participant (participant)
     rhist    = ResidenceHistory.objects.filter_by_participant(participant)
     lang     = LanguageUsage.objects.filter_by_participant(participant)
 
@@ -88,11 +88,11 @@ def show(request, site_id, participant_id):
 
 
 
-    
+
     return render (request, 'browse/participants/show.html', {
         'participant':          participant,
         'site':                 site,
-        'site_id':              site_id, 
+        'site_id':              site_id,
         'sessions':             sessions,
         'residential_history':  rhist,
         'language_usage':       language_usage,
@@ -104,5 +104,3 @@ def show(request, site_id, participant_id):
         'scope' : 'browse'
 
     })
-
-
