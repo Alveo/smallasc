5cd35212de3b7623631e93f711047c6d6585091c
Michael Bauer <da1da2da3ofme@gmail.com>
2017-02-28 18:04:35 +1100

    Added routing and functions to use with the OAuth2 process

--- baseapp/views/security.py
+++ baseapp/views/security.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponseRedirect
 from django.contrib.auth import logout
 from django.conf import settings
+from django.contrib.auth import authenticate
+import pyalveo
 
 def logout_page (request, redirect_url = '/'):
 	logout (request)
@@ -8,5 +10,28 @@ def logout_page (request, redirect_url = '/'):
 
 
 def oauth_callback(request, redirect_url= '/'):
-	settings.PYALVEO_CLIENT.oauth.on_callback(request.build_absolute_uri())
+	client = request.session.get('client',None)
 	
+	if client==None:
+		return HttpResponseRedirect(redirect_url)
+	
+	client.oauth.on_callback(request.build_absolute_uri())
+	
+	
+	return HttpResponseRedirect(redirect_url)
+	
+	
+def oauth_login(request, redirect_url= '/'):
+	
+	client = request.session.get('client',None)
+	
+	if client != None:
+		if client.oauth.validate():
+			return HttpResponseRedirect(redirect_url)
+	
+	client = pyalveo.Client(api_url=settings.API_URL,client_id=settings.OAUTH_CLIENT_ID,
+						client_secret=settings.OAUTH_CLIENT_SECRET,redirect_url=settings.OAUTH_REDIRECT_URL,verifySSL=False)
+	url = client.oauth.get_authorisation_url()
+	request.session['client'] = client
+	redirect_url = url
+	return HttpResponseRedirect(redirect_url)
