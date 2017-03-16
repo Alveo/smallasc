01bacff2e5d745229b23dcdcfe55719ca3967681
Michael Bauer <da1da2da3ofme@gmail.com>
2017-03-16 15:48:53 +1100

    Updated some views with new way of querying.

--- browse/views/sites.py
+++ browse/views/sites.py
@@ -2,15 +2,17 @@ from django.contrib.auth.decorators import login_required, permission_required
 from django.shortcuts               import render
 
 from baseapp.helpers                import generate_paginated_object
-from browse.modelspackage.sites     import Site
+from browse.modelspackage.sites     import Site,SiteManager
 
 from baseapp.helpers import generate_breadcrumbs
 
 @login_required
 @permission_required('auth.can_view_sites')
 def index(request):
+    
+    siteManager = SiteManager(client=request.session.get('client',None))
 
-    sites = Site.objects.all()
+    sites = siteManager.all()
 
     return render (request, 'browse/sites/index.html', {
         'request': request,
