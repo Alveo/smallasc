39993dccb21c26eab9844223ed100f802a1d65f6
Michael Bauer <da1da2da3ofme@gmail.com>
2018-05-16 14:32:35 +1000

    added last_login to User() in create_user and fixed indentation (heaps of warnings)

--- participantportal/modelspackage/auth.py
+++ participantportal/modelspackage/auth.py
@@ -5,75 +5,78 @@ from participantportal.helpers import read_model_property
 from participantportal.models import UserProfile
 
 from django.conf import settings
+
+import time
   
 class CustomAuthBackend(object):
-  """ This custom authenticator is plugged into authentication pipeline
-  and is configured in settings.py."""
-  
-  
-  #supports_inactive_user = True
-
-
-  def authenticate(self, colour, animal, birth_year = None, gender = None):
-    #print "Custom Authenticator Invoked with params %s-%s-%s-%s" % (colour, animal, birth_year, gender)
-    username = "%s_%s" % (colour, animal)
-
-    participantManager = ParticipantManager(client_json=settings.PPCLIENT)
-
-    participant = participantManager.get(username)
-
-    if not participant is None:
-      # Before creating the user check to make sure the
-      # gender and birth year match
-      if birth_year == read_model_property(participant, 'birthYear') and gender == read_model_property(participant, 'gender'):
-        try:
-          user = User.objects.get(username = username)
-        except User.DoesNotExist:
-          user = self.create_user(username)
-
+    """ This custom authenticator is plugged into authentication pipeline
+    and is configured in settings.py."""
+    
+    
+    #supports_inactive_user = True
+    
+    
+    def authenticate(self, colour, animal, birth_year = None, gender = None):
+        #print "Custom Authenticator Invoked with params %s-%s-%s-%s" % (colour, animal, birth_year, gender)
+        username = "%s_%s" % (colour, animal)
+        
+        participantManager = ParticipantManager(client_json=settings.PPCLIENT)
+        
+        participant = participantManager.get(username)
+        
+        if not participant is None:
+            # Before creating the user check to make sure the
+            # gender and birth year match
+            if birth_year == read_model_property(participant, 'birthYear') and gender == read_model_property(participant, 'gender'):
+                try:
+                    user = User.objects.get(username = username)
+                except User.DoesNotExist:
+                    user = self.create_user(username)
+            
+                return user
+    
+        return None
+ 
+    @transaction.atomic  
+    def create_user(self, username):
+        user = User(username = username, is_staff = False, is_active = True, 
+                    is_superuser = False, last_login=time.time())
+        user.set_unusable_password()
+        user.save()
+        
+        self.add_user_to_participant_group(user)
+        
+        up = UserProfile.objects.create(user = user)
+        up.save()
+        up.initialise_agreement()
         return user
 
-    return None
- 
-  @transaction.atomic  
-  def create_user(self, username):
-    user = User(username = username, is_staff = False, is_active = True, is_superuser = False)
-    user.set_unusable_password()
-    user.save()
 
-    self.add_user_to_participant_group(user)
-
-    up = UserProfile.objects.create(user = user)
-    up.save()
-    up.initialise_agreement()
-    return user
-
-
-  def add_user_to_participant_group(self, user):
-    participant_group = Group.objects.get(name = 'participants')
-    user.groups.add(participant_group)
-
-
-  def get_user(self, user_id):
-    try:
-      return User.objects.get(pk = user_id)
-    except User.DoesNotExist:
-      return None
+    def add_user_to_participant_group(self, user):
+        participant_group = Group.objects.get(name = 'participants')
+        user.groups.add(participant_group)
+    
+    
+    def get_user(self, user_id):
+        try:
+            return User.objects.get(pk = user_id)
+        except User.DoesNotExist:
+            return None
 
   
-  # First implementation of Password Reset
-  '''
-
-  def authenticate_password_reset(self, site_label, birth_year = None, gender = None):
-
-    site = Site.objects.get(label=site_label)
-    participants = Participant.objects.all(site)
-
-    for participant in participants:
-      if birth_year == read_model_property(participant, 'birthYear') and gender == read_model_property(participant, 'gender'): #and mother_qual == read_model_property(participant, 'mother_education_level'):
-        #user = User.objects.get(username = read_model_property(participant, 'id'))
-        #return user
-        return participant
-
-    return None
-  '''
+    # First implementation of Password Reset
+    '''
+    
+    def authenticate_password_reset(self, site_label, birth_year = None, gender = None):
+    
+      site = Site.objects.get(label=site_label)
+      participants = Participant.objects.all(site)
+    
+      for participant in participants:
+        if birth_year == read_model_property(participant, 'birthYear') and gender == read_model_property(participant, 'gender'): #and mother_qual == read_model_property(participant, 'mother_education_level'):
+          #user = User.objects.get(username = read_model_property(participant, 'id'))
+          #return user
+          return participant
+    
+      return None
+    '''
--- participantportal/views/session.py
+++ participantportal/views/session.py
@@ -13,74 +13,74 @@ from browse.modelspackage.sites import SiteManager
 from browse.modelspackage.participants import *
 
 def login_page(request):
-  if request.method == 'POST':
-    # When we receive the details we use our custom authenticator located in
-    # the package participantportal.modelspackage.auth
-    form = LoginForm(request.POST)
-    if form.is_valid():
-      # Our request should be routed to our custom authenticator
-      # as configured in our AUTHENTICATION_BACKENDS field of settings.py
-      user = authenticate(
-        colour      = form.data['colour'], 
-        animal      = form.data['animal'],
-        birth_year  = form.cleaned_data['birth_year'],
-        gender      = form.data['gender'])
-
-      if user is not None:
-        login(request, user)
-        return HttpResponseRedirect ('/participantportal/')
-      else:
-        form._errors.setdefault(NON_FIELD_ERRORS, ErrorList())
-  else:
-    form = LoginForm()
-
-  return render(request,'login.html', {'form': form}) 
+    if request.method == 'POST':
+        # When we receive the details we use our custom authenticator located in
+        # the package participantportal.modelspackage.auth
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            # Our request should be routed to our custom authenticator
+            # as configured in our AUTHENTICATION_BACKENDS field of settings.py
+            user = authenticate(
+              colour      = form.data['colour'], 
+              animal      = form.data['animal'],
+              birth_year  = form.cleaned_data['birth_year'],
+              gender      = form.data['gender'])
+            
+            if user is not None:
+                login(request, user)
+                return HttpResponseRedirect ('/participantportal/')
+            else:
+                form._errors.setdefault(NON_FIELD_ERRORS, ErrorList())
+    else:
+        form = LoginForm()
+    
+    return render(request,'login.html', {'form': form}) 
 
 # view to display the possible Colour-Animal Combination after the user answers a couple of questions
 def password_reset(request):
   
-  if request.method == 'POST':
-    form = ColourAnimalHelperForm(request.POST)
-    
+    if request.method == 'POST':
+        form = ColourAnimalHelperForm(request.POST)
         
-    if form.is_valid():
-        # Create a SPARQL Query to retrieve possible results based on the answers
-
-        siteManager = SiteManager(client_json=request.session.get('client',None))
-
-        # Get the site object from site_label (get function needs a label parameter)
-        site_label = form.data['pwd_site'] 
-        site = siteManager.get(label=site_label)
         
-        query = '?part austalk:recording_site <%s> .\n' % site.identifier
-
-        # Grab all the form data and convert them to lower case
-        # in order to compare the result of SPARQL Query
-        
-        if form.data['pwd_highest_qual'] != 'None':
-            value = form.data['pwd_highest_qual']
-            query += "?part austalk:education_level '%s' .\n" % value
+        if form.is_valid():
+            # Create a SPARQL Query to retrieve possible results based on the answers
             
-        
-        if form.data['pwd_father_highest_qual'] != 'None':
-            value = form.data['pwd_father_highest_qual']
-            query += "?part austalk:father_education_level '%s' .\n" % value 
- 
-        
-        if form.data['pwd_mother_highest_qual'] != 'None':
-            value = form.data['pwd_mother_highest_qual']
-            query += "?part austalk:mother_education_level '%s' .\n" % value 
-        
-        sparql = SparqlManager()
-
-        sparql_results = sparql.query("""
-                SELECT  ?part 
-                where {
-                    ?part rdf:type foaf:Person . 
-                    %s
-                }""" % query)
-
-        results = []
+            siteManager = SiteManager(client_json=request.session.get('client',None))
+            
+            # Get the site object from site_label (get function needs a label parameter)
+            site_label = form.data['pwd_site'] 
+            site = siteManager.get(label=site_label)
+            
+            query = '?part austalk:recording_site <%s> .\n' % site.identifier
+            
+            # Grab all the form data and convert them to lower case
+            # in order to compare the result of SPARQL Query
+            
+            if form.data['pwd_highest_qual'] != 'None':
+                value = form.data['pwd_highest_qual']
+                query += "?part austalk:education_level '%s' .\n" % value
+                
+            
+            if form.data['pwd_father_highest_qual'] != 'None':
+                value = form.data['pwd_father_highest_qual']
+                query += "?part austalk:father_education_level '%s' .\n" % value 
+            
+            
+            if form.data['pwd_mother_highest_qual'] != 'None':
+                value = form.data['pwd_mother_highest_qual']
+                query += "?part austalk:mother_education_level '%s' .\n" % value 
+            
+            sparql = SparqlManager()
+            
+            sparql_results = sparql.query("""
+                    SELECT  ?part 
+                    where {
+                        ?part rdf:type foaf:Person . 
+                        %s
+                    }""" % query)
+            
+            results = []
         
         # Bind the result from SPARQL query into a list - results            
         for result in sparql_results["results"]["bindings"]:
@@ -89,15 +89,15 @@ def password_reset(request):
                 identifier          = result["part"]["value"]
                 
             )
-
+            
             results.append (part)
-
+        
         # return the results to the template for displaying to the user
         return render(request,'colour_animal_helper_done.html', {'results': results }) 
-
-  else:
-    form = ColourAnimalHelperForm()
-  
-  return render(request, 'colour_animal_helper_form.html', {'form': form}) 
+    
+    else:
+        form = ColourAnimalHelperForm()
+    
+    return render(request, 'colour_animal_helper_form.html', {'form': form}) 
 
 
